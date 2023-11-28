c59e2f8e96a882c0d2903b0aea4494308c60ac04
VALADI K JAGANATHAN <info@orchidsasia.com>
2023-11-28 15:05:13 +0530

    Update _init_.py

--- _init_.py
+++ _init_.py
@@ -141,10 +141,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
-#for node in os.listdir(os.path.dirname(__file__)):
- #   if node.startswith('EXT_'):
- #       node = node.split('.')[0]
-  #      node_import = importlib.import_module('custom_nodes.SampleDiffusion.', node)
-        # get class node mappings from py file
   #      NODE_CLASS_MAPPINGS.update(node_import.NODE_CLASS_MAPPINGS)
 
